d31a6ddbf343f6a98270486af889746dde1d0df4
guilh-n-l <guilherme.lima@sou.inteli.edu.br>
2024-02-25 20:08:54 -0300

    [FIX] Fixing delete bug

--- frontend/app.js
+++ frontend/app.js
@@ -13,23 +13,33 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/data', async (_, res) => {
-    const data = await http.get(`${BACKEND_URL}/dogs`)
-    const html = data.map(i=> format_json(i)).join('')
-    res.send(html)
-})
+app.route('/data')
+    .get(async (_, res) => {
+        const data = await http.get(`${BACKEND_URL}/dogs`)
+        const html = data.map(i => format_json(i)).join('')
+        res.send(html)
+    })
+    .post(async (req, res) => {
+        const object = req.body;
+        const data = await http.post(`${BACKEND_URL}/dogs`, format_post(req.body));
+        if (data[0] !== "error") {
+            res.status(500);
+        }
+        else {
+            res.status(200);
+        }
+    });
 
-app.post('/send', async (req, res) => {
-    const object = req.body
-    const data = await http.post(`${BACKEND_URL}/dogs`, format_post(req.body))
+app.delete('/data/id=:id', async (req, res) => {
+    const data = await http.del(`${BACKEND_URL}/dogs/id=${req.params.id}`); 
     if (data[0] !== "error") {
-        res.status(204).send('')
+        res.status(500);
     }
     else {
-        res.status(200).send('')
+        res.status(200);
     }
-})
+});
 
 app.listen(port, () => {
-    console.log(`${BACKEND_URL} Running at port ${port}`)
-})
+    console.log(`${BACKEND_URL} Running at port ${port}`);
+});
--- frontend/services/http-requests.js
+++ frontend/services/http-requests.js
@@ -12,14 +12,25 @@ let get = async (url) => {
 
 let post = async (url, body) => {
     try {
-        console.log(body)
         const response = await axios.post(url, body)
         return response.data
     } catch (e) {
-        console.log(e)
-        return ['error']
+        console.log(e);
+        return ['error'];
     }
 }
 
-exports.get = get
-exports.post = post
+let del = async (url) => {
+    try {
+        console.log(url)
+        const response = await axios.delete(url);
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        return ['error'];
+    }
+}
+
+exports.get = get;
+exports.post = post;
+exports.del = del;
